2c5f19625cfaaea57c9597748f0f0770a9c87e2f
Potiphargvaye <potiphargvaye@gmail.com>
2025-09-12 04:18:01 +0200

    Add built Vite assets

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,12 @@ export default defineConfig({
     build: {
         manifest: true,
         outDir: 'public/build',
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+            },
+        },
     },
-}); 
+});
