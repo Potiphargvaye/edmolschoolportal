f8d814537d700ec3a2c5e43473047287f8988c79
Potiphargvaye <potiphargvaye@gmail.com>
2025-09-16 21:11:25 +0200

    Fresh Update nuber 3

--- resources/views/admin/students/create.blade.php
+++ resources/views/admin/students/create.blade.php
@@ -190,9 +190,9 @@
                             <label for="gender" class="form-label">Gender <span class="text-red-500">*</span></label>
                             <select class="form-input @error('gender') border-red-500 @enderror" id="gender" name="gender" required>
                                 <option value="">Select Gender</option>
-                                <option value="male" {{ old('gender') == 'male' ? 'selected' : '' }}>Male</option>
-                                <option value="female" {{ old('gender') == 'female' ? 'selected' : '' }}>Female</option>
-                                <option value="0ther" {{ old('gender') == 'other' ? 'selected' : '' }}>Other</option>
+                                <option value="male" {{ old('gender') == 'male' ? 'selected' : '' }}>male</option>
+                                <option value="female" {{ old('gender') == 'female' ? 'selected' : '' }}>female</option>
+                                <option value="other" {{ old('gender') == 'other' ? 'selected' : '' }}>other</option>
                             </select>
                             @error('gender')
                                 <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
